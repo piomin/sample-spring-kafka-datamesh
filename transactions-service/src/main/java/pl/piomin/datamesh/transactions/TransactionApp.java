45f4950ba8389c26c9990b378f45bf0c0d9cb2fb
piomin <piotr.minkowski@gmail.com>
2023-09-18 14:29:43 +0200

    fix errors

--- transactions-service/src/main/java/pl/piomin/datamesh/transactions/TransactionApp.java
+++ transactions-service/src/main/java/pl/piomin/datamesh/transactions/TransactionApp.java
@@ -1,14 +1,26 @@
 package pl.piomin.datamesh.transactions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.context.annotation.Bean;
+import pl.piomin.datamesh.transactions.domain.Transaction;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class TransactionApp {
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionApp.class, args);
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(TransactionApp.class);
+
+    @Bean
+    public Consumer<Transaction> transactions() {
+        return trx -> LOG.info("Received: {}", trx);
     }
 
 }
