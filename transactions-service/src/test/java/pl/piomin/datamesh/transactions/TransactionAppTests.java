034afb3207d7b9d5b8a50abb9ba51063066a3766
piomin <piotr.minkowski@gmail.com>
2026-03-09 11:32:17 +0100

    update testconatners + kafka

--- transactions-service/src/test/java/pl/piomin/datamesh/transactions/TransactionAppTests.java
+++ transactions-service/src/test/java/pl/piomin/datamesh/transactions/TransactionAppTests.java
@@ -6,37 +6,35 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(properties = "springwolf.enabled=false")
 @Testcontainers
 public class TransactionAppTests {
 
-    private static Network NET = Network.newNetwork();
-
     @Container
     @ServiceConnection
-    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"))
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.0"))
 //            .withKraft()
             .withExposedPorts(9093, 9092)
-            .withCreateContainerCmdModifier(cmd -> cmd.withName("broker"))
-            .withNetwork(NET);
+            .withNetwork(Network.SHARED)
+            .withNetworkAliases("broker");
 
     @Container
     static final GenericContainer ksqlServer = new GenericContainer("confluentinc/ksqldb-server:0.26.0")
             .dependsOn(kafka)
             .withExposedPorts(8088)
-            .withNetwork(NET)
+            .withNetwork(Network.SHARED)
             .withEnv("KSQL_BOOTSTRAP_SERVERS", "broker:9092")
             .withEnv("KSQL_LISTENERS", "http://0.0.0.0:8088");
 
     @DynamicPropertySource
     static void kafkaProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+//        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
         registry.add("spring.ksql-server.host", ksqlServer::getHost);
         registry.add("spring.ksql-server.port", () -> String.valueOf(ksqlServer.getMappedPort(8088)));
     }
